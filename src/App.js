d5f69a82a36f53dad77ea62b12a2d0b9a225f754
Sean Berry <seanberry@live.co.uk>
2024-05-29 16:59:21 +0100

    Nav component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import './App.css';
+import Nav from './Components/Nav';
 
 function App() {
   return (
-    <div className="App">
-   // Homepage
-    </div>
+    <>
+    <Nav />
+    </>
   );
 }
 
---
+++ src/Components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <a href='/'>
+            </a>
+        </nav>
+    )
+}
+
+export default Nav;
